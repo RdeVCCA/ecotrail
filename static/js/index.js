1f92b3497d1b6a02addd89c8b36d29519906419b
TingAn1212 <105140568+TingAn1212@users.noreply.github.com>
2024-03-20 17:43:54 +0800

    animation on scroll and partial video background code

--- static/js/index.js
+++ static/js/index.js
@@ -1,17 +1,59 @@
 const video = $('#video');
 const content = $('#video-content');
-
-var videoPlaying = false;
-var lastScrollTop = 0;
+const timeList = [];
+var timeIndex = 0;
 var time = 0;
+var targetTime = 0;
+var reversePlay = false;
+var reverseInterval;
 video[0].pause();
 
-window.addEventListener('wheel', function(e) {
-    const scrollDelta = e.deltaY;
-    // Map the scroll position to a time in the video
-    time += scrollDelta * 0.01;
-    console.log(time, video[0].duration);
-    if (time >= 0 && time <= video[0].duration) {
-        video[0].currentTime = time;
-    }
+video[0].currentTime = 100;
+
+var cooldown = 0;
+
+video[0].addEventListener('seeked', function() {
+    seeking = false;
 });
+
+async function reverseControl() {
+    while (true) {
+        if (reversePlay && !seeking) {
+            video[0].pause();
+            if (video[0].currentTime > targetTime) {
+                seeking = true;
+                video[0].currentTime -= 0.1;
+            } else {
+                reversePlay = false;
+            }
+        }
+        await new Promise(r => setTimeout(r, 50));
+    }
+}
+
+function playVideoUntil(time){
+    video[0].play();
+    targetTime = time;
+    if (targetTime > video[0].position) {
+        reversePlay = false;
+    } else {
+        reversePlay = true;
+    }
+}
+
+window.addEventListener('wheel', function(e) {
+    let position = window.scrollY;
+    let videoPosition = video.offset().top;
+    if(position > videoPosition){
+        cooldown = 0.5;
+        timeIndex++;
+        if (timeIndex >= timeList.length) {
+            timeIndex = timeList.length - 1;
+        }
+        let prevTime = time;
+        time = timeList[timeIndex];
+    }
+
+});
+
+reverseControl();
