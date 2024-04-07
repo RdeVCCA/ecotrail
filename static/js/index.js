d619cce74a1c53cc27bb762736810af622ca0cb9
TingAn1212 <105140568+TingAn1212@users.noreply.github.com>
2024-04-07 15:49:32 +0800

    video control
    use playVideoUntilNext(-1) and playVideoUntilNext(1)
    
    to control time step

--- static/js/index.js
+++ static/js/index.js
@@ -8,17 +8,28 @@ var reversePlay = false;
 var reverseInterval;
 video[0].pause();
 
-video[0].currentTime = 100;
-
+video[0].currentTime = 0;
 var cooldown = 0;
 
-video[0].addEventListener('seeked', function() {
-    seeking = false;
-});
+var timeStep = [0, 1, 2, 3, 4, 5];
+var playing = false;
+var timeStepIndex = 0;
+
+function calculateIndex(increase) {
+    var bottomLimit = 0;
+    var upperLimit = timeStep.length - 1;
+    var nextIndex = timeStepIndex + increase;
+    if (nextIndex < bottomLimit) {
+        nextIndex = bottomLimit;
+    } else if (nextIndex > upperLimit) {
+        nextIndex = upperLimit;
+    }
+    return nextIndex;
+}
 
 async function reverseControl() {
     while (true) {
-        if (reversePlay && !seeking) {
+        if (reversePlay && playing) {
             video[0].pause();
             if (video[0].currentTime > targetTime) {
                 seeking = true;
@@ -27,18 +38,59 @@ async function reverseControl() {
                 reversePlay = false;
             }
         }
-        await new Promise(r => setTimeout(r, 50));
+        await new Promise(r => setTimeout(r, 100));
     }
 }
 
-function playVideoUntil(time){
-    video[0].play();
-    targetTime = time;
-    if (targetTime > video[0].position) {
+async function playVideoUntilNext(increase){
+    var newIndex = calculateIndex(increase);
+    targetTime = timeStep[newIndex];
+
+    if (newIndex == timeStepIndex) {
+        return;
+    }
+    console.log(targetTime, video[0].currentTime)
+    if (targetTime >= video[0].currentTime) {
         reversePlay = false;
     } else {
         reversePlay = true;
     }
+
+    video[0].play();
+    playing = true;
+
+    if (!reversePlay) {
+        console.log(video[0].currentTime, targetTime)
+        while (true){
+            // console.log("test forward")
+            if (video[0].currentTime >= targetTime) {
+                video[0].currentTime = targetTime;
+                video[0].pause();
+                playing = false;
+                timeStepIndex = newIndex;
+                break;
+            }else{
+                console.log(video[0].currentTime, targetTime)
+            }
+            await new Promise(r => setTimeout(r, 100));
+        }
+    }else{
+        console.log(video[0].currentTime, targetTime)
+        while (true){
+            // console.log("test backward")
+            if (video[0].currentTime <= targetTime) {
+                video[0].currentTime = targetTime;
+                video[0].pause();
+                playing = false;
+                timeStepIndex = newIndex;
+                break;
+            }else{
+                console.log(video[0].currentTime, targetTime)
+            }
+            await new Promise(r => setTimeout(r, 100));
+        }
+    }
+    time = targetTime;
 }
 
 window.addEventListener('wheel', function(e) {
