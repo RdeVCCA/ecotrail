d08cb166df172af6f964a95f0825e446a9350f41
LarissaLiou <110041401+LarissaLiou@users.noreply.github.com>
2024-05-08 11:06:23 +0800

    fixed css and added javascript animations
    - added sections and fixed all to 100vh
    - fixed padding and margin
    
    - added animations for header in load in
    - added animations for fade in upon scroll
    - added animations for float left and right upon scroll
    
    - not sure if it takes a wihle to load for all users but it happened after i added the floatleft animation
    
    - will fix mobile view after

---
+++ static/js/wetlands.js
@@ -0,0 +1,83 @@
+window.addEventListener("scroll", getPosition);
+window.addEventListener("resize", getPosition);
+window.addEventListener("scroll", fadeIn);
+window.addEventListener("resize", fadeIn);
+
+//does fading for each section upon scroll
+function getPosition(){
+    const htmlElement = document.documentElement;
+    positions = document.getElementsByTagName('section');
+    for (position of positions){
+        rect = position.getBoundingClientRect();
+        positionOpacity = (rect.top / htmlElement.clientHeight * 100);
+        if (positionOpacity < 50){
+            position.style.opacity = 100 + positionOpacity * 2 + '%';
+        }
+    }
+}
+
+//does starting fade in 
+function floatIn1(){ //float in for the text
+    document.getElementById('load1').style.transition = '3s'
+    document.getElementById('load1').style.transform = 'translate(10vh)'
+    document.getElementById('load2').style.transition = '3s'
+    document.getElementById('load2').style.transform = 'translate(10vh)'
+}
+
+function floatIn2(){ //float in for the image
+    document.getElementById('headerimg').style.transition = '3s'
+    document.getElementById('headerimg').style.transform = 'translateY(5vh)'
+}
+
+//check if on screen
+function checkOnScreen(obj) {
+	const rect = obj.getBoundingClientRect();
+	return (
+		(rect.top >= 0 &&
+			rect.top <= window.innerHeight)
+		||
+		(rect.bottom >= 0 &&
+			rect.bottom <= window.innerHeight)
+	);
+}
+
+//paragraph fade ins
+function fadeIn(){
+    fadeList = document.getElementsByClassName('hidden');
+    for (i of fadeList){
+        if (checkOnScreen(i)){
+            i.style.opacity = '100%'
+            i.style.transform = 'scale(1)'
+        } else{
+            i.style.transform = 'scale(0.8)'
+        }
+    }
+
+    floatLeftList = document.getElementsByClassName('floatLeft');
+    for (i of floatLeftList){
+        if (checkOnScreen(i)){
+            i.style.opacity = '100%'
+            i.style.transform = 'translate(8vh)'
+        } else{
+            i.style.transform = 'translate(-8vh)'
+        }
+    }
+
+    floatRightList = document.getElementsByClassName('floatRight');
+    for (i of floatRightList){
+        if (checkOnScreen(i)){
+            i.style.opacity = '100%'
+            i.style.transform = 'translate(-8vh)'
+        } else{
+            i.style.transform = 'translate(8vh)'
+        }
+    }
+}
+
+
+
+//initialise fading onload
+window.onload = setTimeout(floatIn2, 700)
+window.onload = setTimeout(floatIn1, 500);
+
+getPosition()
