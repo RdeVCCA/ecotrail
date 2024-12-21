4766809356ac7a48bc3b20502618a0b376039438
LarissaLiou <110041401+LarissaLiou@users.noreply.github.com>
2024-12-21 22:55:05 +0800

    Remove quick access to areas on mobile

--- static/js/nav.js
+++ static/js/nav.js
@@ -1,16 +1,21 @@
+function isDesktop() {
+    return window.innerWidth >= 768; // Checks if screen width is 768px or smaller (common for phones)
+}
+
 function displayBelow(){
-    var redirectBlock = document.getElementsByClassName("nav2");
-    Array.from(redirectBlock).forEach((element) => {
-        element.style.display = "flex";
-    });
-    console.log("appear")
-    
+    if (isDesktop()) {
+        var redirectBlock = document.getElementsByClassName("nav2");
+        Array.from(redirectBlock).forEach((element) => {
+            element.style.display = "flex";
+        });
+    }
 }
 
 function stopDisplay(){
-    var redirectBlock = document.getElementsByClassName("nav2");
-    Array.from(redirectBlock).forEach((element) => {
-        element.style.display = "none";
-    });
-    console.log("hide")
+    if (isDesktop()) {
+        var redirectBlock = document.getElementsByClassName("nav2");
+        Array.from(redirectBlock).forEach((element) => {
+            element.style.display = "none";
+        });
+    }
 }
